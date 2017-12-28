3630c9d0aadbb4fed5367d7ceb050cbb9ee6b5cf
YiJun Chen <gzenux@gmail.com>
2017-12-28 13:46:10 +0800

    project tree: add support ASUS RT-N18U device - fix board parameters

--- trunk/configs/boards/RT-N18U/board.mk
+++ trunk/configs/boards/RT-N18U/board.mk
@@ -5,4 +5,12 @@
 ##################################################################
 
 CFLAGS += -DBOARD_N18U -DVENDOR_ASUS
+
+# required definitions for this board
 BOARD_NUM_USB_PORTS=2
+BOARD_2G_IN_SOC=0
+BOARD_5G_IN_SOC=0
+BOARD_HAS_5G_RADIO=0
+BOARD_RAM_SIZE=256
+
+# optional customization extra definitions
