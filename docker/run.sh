80463bda5f66874dea42a5d6945693c78e488b07
YiJun Chen <gzenux@gmail.com>
2018-01-04 15:46:03 +0800

    docker: update Dockerfile and run.sh script

--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,52 @@
 #!/usr/bin/env bash
 
-ImageName=padavan:rtn18u
-HostName=padavan
-SrcDir=""
+ImageName=padavan
+ImageTag=rtn18u
+SrcName=${ImageName}
+HostName=${ImageName}
+Image=${ImageName}:${ImageTag}
 
-pushd $(dirname $0)/../ && SrcDir=$(pwd) && popd || { echo "Fail to retrieve the source directory path."; exit 1; }
+function usage() {
+	cat <<EOF >&2
+Usage: $(basename $0) [options]
 
-[ "$(docker images -q ${ImageName})" == "" -o "$1" == "update" ] && {
+Options:
+   -u|--update
+      update/rebuild the docker image before run this image
+      default: false
+   -f|--force
+      force rebuild the docker image without docker cache
+      default: false
+   -h|--help
+      show this usage
+
+EOF
+}
+
+# options
+Update=0
+Force=0
+Help=0
+Opts=$(getopt -o ufh --long update,force,help -- "$@")
+[[ $? != 0 ]] && { usage; exit 1; }
+eval set -- "$Opts"
+while true; do
+	case "$1" in
+		-u|--update) Update=1; shift;;
+		-f|--force)  Update=1; Force=1; shift;;
+		-h|--help)   Help=1; shift;;
+		--)          shift; break;;
+		*) echo "Arguments parsing error"; exit 1;;
+	esac
+done
+
+[[ "$Help" == 1 ]] && { usage; exit 0; }
+
+[[ "$(docker images -q ${Image})" == "" || "$Update" == 1 ]] && {
 	pushd $(dirname $0)
-	docker build --build-arg HOME=${HOME} --build-arg USER=${USER} --build-arg UID=${UID} -t ${ImageName} .
+	[[ "$Force" == 1 ]] && buildopts="--no-cache"
+	docker build ${buildopts} --build-arg HOME=${HOME} --build-arg USER=${USER} --build-arg UID=${UID} -t ${Image} .
 	popd
 }
 
-docker run --rm --hostname ${HostName} -v ${SrcDir}:${HOME}/$(basename ${SrcDir}) -it ${ImageName}
+docker run --rm --hostname ${HostName} -v $(pwd):${HOME}/${SrcName} -it ${Image}
