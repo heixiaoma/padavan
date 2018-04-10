8968e241fecd9388149eae5a46eee1156e8ed5c1
YiJun Chen <gzenux@gmail.com>
2018-04-10 14:24:02 +0800

    docker: update Dockerfile and run.sh script
    1. new some options to make run.sh more flexible
    2. use bash as default shell
    3. add environment variables: USER, LANG and LANGUAGE
    4. use argument LANGUAGES to keep language list during container build-time

--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,56 @@
 #!/usr/bin/env bash
 
-ImageName=padavan:rtn18u
-HostName=padavan
-SrcDir=""
+ImageName=padavan
+ImageTag=rtn18u
+SrcName=${ImageName}
+HostName=${ImageName}
 
-pushd $(dirname $0)/../ && SrcDir=$(pwd) && popd || { echo "Fail to retrieve the source directory path."; exit 1; }
+function usage() {
+	cat <<EOF >&2
+Usage: $(basename $0) [options]
 
-[ "$(docker images -q ${ImageName})" == "" -o "$1" == "update" ] && {
-	pushd $(dirname $0)
-	docker build --build-arg HOME=${HOME} --build-arg USER=${USER} --build-arg UID=${UID} -t ${ImageName} .
+Options:
+   -u|--update
+      update/rebuild the docker image before run this image
+      default: false
+   -f|--force
+      force rebuild the docker image without docker cache
+      default: false
+   -t|--tag
+      change image tag/folder to build/run the docker image
+      default: latest
+   -h|--help
+      show this usage
+
+EOF
+}
+
+# options
+Update=0
+Force=0
+Help=0
+Opts=$(getopt -o uft:h --long update,force,tag:,help -- "$@")
+[[ $? != 0 ]] && { usage; exit 1; }
+eval set -- "$Opts"
+while true; do
+	case "$1" in
+		-u|--update) Update=1; shift;;
+		-f|--force)  Update=1; Force=1; shift;;
+		-t|--tag)    ImageTag=$2; [[ -d "$(dirname $0)/${ImageTag}" ]] || { echo "$(dirname $0)/${ImageTag} does not exist!"; exit 1; }; shift 2;;
+		-h|--help)   Help=1; shift;;
+		--)          shift; break;;
+		*) echo "Arguments parsing error"; exit 1;;
+	esac
+done
+
+[[ "$Help" == 1 ]] && { usage; exit 0; }
+
+Image=${ImageName}:${ImageTag}
+[[ "$(docker images -q ${Image})" == "" || "$Update" == 1 ]] && {
+	[[ -d "$(dirname $0)/${ImageTag}" ]] && pushd $(dirname $0)/${ImageTag} || pushd $(dirname $0)
+	[[ "$Force" == 1 ]] && buildopts="--no-cache"
+	docker build ${buildopts} --build-arg HOME=${HOME} --build-arg USER=${USER} --build-arg UID=${UID} -t ${Image} .
 	popd
 }
 
-docker run --rm --hostname ${HostName} -v ${SrcDir}:${HOME}/$(basename ${SrcDir}) -it ${ImageName}
+docker run --rm --hostname ${HostName} -v $(pwd):${HOME}/${SrcName} -it ${Image}
