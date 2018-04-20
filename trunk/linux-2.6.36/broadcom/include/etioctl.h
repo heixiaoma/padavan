5de00ac470be20783fbee82ec6bcd4e6812029d7
YiJun Chen <gzenux@gmail.com>
2018-04-20 16:19:03 +0800

    switch_robo: temp save for develop in progress

--- trunk/linux-2.6.36/broadcom/include/etioctl.h
+++ trunk/linux-2.6.36/broadcom/include/etioctl.h
@@ -86,6 +86,155 @@
 #define SIOCGETCROBORD		(SIOCDEVPRIVATE + ETCROBORD)
 #define SIOCSETCROBOWR		(SIOCDEVPRIVATE + ETCROBOWR)
 
+/////////////////////////////////////////////////
+// Common Switch APIs -- begin
+/////////////////////////////////////////////////
+
+// *** VALUES DEFINITION ***
+
+/////////////////////////////////////////////////
+// MAGIC
+/////////////////////////////////////////////////
+
+#define SWAPI_MAGIC_RESET_ASIC			(0x25252525)
+
+/////////////////////////////////////////////////
+// HW INDEPENDENT PORT ID
+/////////////////////////////////////////////////
+
+#define SWAPI_PORT_WAN				0
+#define SWAPI_PORT_LAN1				1
+#define SWAPI_PORT_LAN2				2
+#define SWAPI_PORT_LAN3				3
+#define SWAPI_PORT_LAN4				4
+#define SWAPI_PORT_LAN5				5
+#define SWAPI_PORT_LAN6				6
+#define SWAPI_PORT_LAN7				7
+#define SWAPI_PORT_CPU_INIC			13
+#define SWAPI_PORT_CPU_LAN			14
+#define SWAPI_PORT_CPU_WAN			15
+
+/////////////////////////////////////////////////
+// HW INDEPENDENT PORT MASK
+/////////////////////////////////////////////////
+
+#define SWAPI_PORTMASK_WAN			(1u<<SWAPI_PORT_WAN)
+#define SWAPI_PORTMASK_LAN1			(1u<<SWAPI_PORT_LAN1)
+#define SWAPI_PORTMASK_LAN2			(1u<<SWAPI_PORT_LAN2)
+#define SWAPI_PORTMASK_LAN3			(1u<<SWAPI_PORT_LAN3)
+#define SWAPI_PORTMASK_LAN4			(1u<<SWAPI_PORT_LAN4)
+#define SWAPI_PORTMASK_LAN5			(1u<<SWAPI_PORT_LAN5)
+#define SWAPI_PORTMASK_LAN6			(1u<<SWAPI_PORT_LAN6)
+#define SWAPI_PORTMASK_LAN7			(1u<<SWAPI_PORT_LAN7)
+#define SWAPI_PORTMASK_CPU_INIC			(1u<<SWAPI_PORT_CPU_INIC)
+#define SWAPI_PORTMASK_CPU_LAN			(1u<<SWAPI_PORT_CPU_LAN)
+#define SWAPI_PORTMASK_CPU_WAN			(1u<<SWAPI_PORT_CPU_WAN)
+
+/////////////////////////////////////////////////
+// BRIDGE MODES
+/////////////////////////////////////////////////
+
+#define SWAPI_WAN_BRIDGE_DISABLE		(0)
+#define SWAPI_WAN_BRIDGE_LAN1			(1)
+#define SWAPI_WAN_BRIDGE_LAN2			(2)
+#define SWAPI_WAN_BRIDGE_LAN3			(3)
+#define SWAPI_WAN_BRIDGE_LAN4			(4)
+#define SWAPI_WAN_BRIDGE_LAN3_LAN4		(5)
+#define SWAPI_WAN_BRIDGE_LAN1_LAN2		(6)
+#define SWAPI_WAN_BRIDGE_LAN1_LAN2_LAN3		(7)
+#define SWAPI_WAN_BRIDGE_DISABLE_WAN		(8)
+#define SWAPI_WAN_BRIDGE_NUM			(SWAPI_WAN_BRIDGE_DISABLE_WAN+1)
+
+/////////////////////////////////////////////////
+// BRIDGE WAN ISOLATION MODES
+/////////////////////////////////////////////////
+
+#define SWAPI_WAN_BWAN_ISOLATION_NONE		(0)
+///////////////////////////////////////////
+//     CPU_EXTIF_0           CPU_EXTIF_1
+//     |         |               |
+//    WAN <--> BWAN(LANX)    LAN(others)
+///////////////////////////////////////////
+
+#define SWAPI_WAN_BWAN_ISOLATION_FROM_CPU	(1)
+///////////////////////////////////////////
+//     CPU_EXTIF_0           CPU_EXTIF_1
+//     |                         |
+//    WAN <--> BWAN(LANX)    LAN(others)
+///////////////////////////////////////////
+
+#define SWAPI_WAN_BWAN_ISOLATION_BETWEEN	(2)
+///////////////////////////////////////////
+//     CPU_EXTIF_0           CPU_EXTIF_1
+//     |         |               |
+//    WAN      BWAN(LANX)    LAN(others)
+///////////////////////////////////////////
+
+/////////////////////////////////////////////////
+// VLAN MODES
+/////////////////////////////////////////////////
+
+#define SWAPI_VLAN_ACCEPT_FRAMES_ALL		(0)
+#define SWAPI_VLAN_ACCEPT_FRAMES_TAG_ONLY	(1)
+#define SWAPI_VLAN_ACCEPT_FRAMES_UNTAG_ONLY	(2)
+
+#define SWAPI_VLAN_RULE_WAN_INET		(0)
+#define SWAPI_VLAN_RULE_WAN_IPTV		(1)
+#define SWAPI_VLAN_RULE_WAN_LAN1		(2)
+#define SWAPI_VLAN_RULE_WAN_LAN2		(3)
+#define SWAPI_VLAN_RULE_WAN_LAN3		(4)
+#define SWAPI_VLAN_RULE_WAN_LAN4		(5)
+#define SWAPI_VLAN_RULE_NUM			(SWAPI_VLAN_RULE_WAN_LAN4+1)
+
+/////////////////////////////////////////////////
+// LINK MODES
+/////////////////////////////////////////////////
+
+#define SWAPI_LINK_SPEED_MODE_AUTO		(0)
+#define SWAPI_LINK_SPEED_MODE_AUTO_1000_FD	(1)
+#define SWAPI_LINK_SPEED_MODE_AUTO_100_FD	(2)
+#define SWAPI_LINK_SPEED_MODE_AUTO_100_HD	(3)
+#define SWAPI_LINK_SPEED_MODE_AUTO_10_FD	(4)
+#define SWAPI_LINK_SPEED_MODE_AUTO_10_HD	(5)
+#define SWAPI_LINK_SPEED_MODE_FORCE_100_FD	(6)
+#define SWAPI_LINK_SPEED_MODE_FORCE_100_HD	(7)
+#define SWAPI_LINK_SPEED_MODE_FORCE_10_FD	(8)
+#define SWAPI_LINK_SPEED_MODE_FORCE_10_HD	(9)
+#define SWAPI_LINK_SPEED_MODE_FORCE_POWER_OFF	(15)
+
+#define SWAPI_LINK_FLOW_CONTROL_TX_RX		(0)
+#define SWAPI_LINK_FLOW_CONTROL_TX_ASYNC	(1)
+#define SWAPI_LINK_FLOW_CONTROL_DISABLE		(2)
+
+/////////////////////////////////////////////////
+// LED MODES
+/////////////////////////////////////////////////
+
+#define SWAPI_LED_PHYMODE_1000_ACT		(0)
+#define SWAPI_LED_PHYMODE_100_ACT		(1)
+#define SWAPI_LED_PHYMODE_10_ACT		(2)
+#define SWAPI_LED_PHYMODE_100_10_ACT		(3)
+#define SWAPI_LED_PHYMODE_1000			(4)
+#define SWAPI_LED_PHYMODE_100			(5)
+#define SWAPI_LED_PHYMODE_10			(6)
+#define SWAPI_LED_LINK_ACT			(7)
+#define SWAPI_LED_LINK_ACT_RX			(8)
+#define SWAPI_LED_LINK_ACT_TX			(9)
+#define SWAPI_LED_DUPLEX_COLLISION		(10)
+#define SWAPI_LED_OFF				(11)
+
+/////////////////////////////////////////////////
+
+typedef struct port_bytes_s
+{
+	uint64_t RX;
+	uint64_t TX;
+} port_bytes_t;
+
+/////////////////////////////////////////////////
+// Common Switch APIs -- end
+/////////////////////////////////////////////////
+
 /* structure to send a generic var set/get */
 typedef struct et_var_s {
 	uint cmd;
